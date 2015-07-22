2341df06974948f550b80d778905410e64355083
Hackbright Student <info@hackbrightacademy.com>
2015-07-22 12:58:35 -0700

    work in progress - flask

--- hackbright-web.py
+++ hackbright-web.py
@@ -14,7 +14,7 @@ def get_student_form():
 def get_student():
     """Show information about a student."""
 
-    github = request.args.get('github', 'jhacks')
+    github = request.args.get('github', 'jhacks') #default values
     first, last, github = hackbright.get_student_by_github(github)
     html = render_template("student_info.html", 
                             first=first, 
@@ -22,10 +22,31 @@ def get_student():
                             github=github)
     return html
 
-@app.route("/student-add")
+@app.route("/input-new-student")
 def student_add():
-    """Add a student."""
-    return render_template("student_info.html")
+    """Input info for a new student."""
+    return render_template("input_new_student.html")
+
+    first = request.args.get('first')
+    last = request.args.get('last')
+    github = request.args.get('github')
+
+    html = render_template("add-student-toDB.html", 
+                            first=first, 
+                            last=last,
+                            github=github)
+    return html
+
+@app.route("/add-student-toDB")
+def new_student():
+    """Add new student to DB, and show information."""
+
+    hackbright.make_new_student(first, last, github)
+    return render_template("add_student_toDB.html",
+                            first=first,
+                            last=last,
+                            github=github)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
